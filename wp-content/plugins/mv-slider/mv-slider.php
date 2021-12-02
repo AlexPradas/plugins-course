8c00e14c04f27ce2e8873b55fbb591765a830718
Alex <alferpra@gmail.com>
2021-12-02 12:49:36 +0100

    Create mv-slider structure

---
+++ wp-content/plugins/mv-slider/mv-slider.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: MV Slider
+ * Plugin URI: https://www.wordpress.org/mv-slider
+ * Description: My plugin's description
+ * Version: 1.0
+ * Requires at least: 5.6
+ * Author: Alex Pradas
+ * Author URI: https://alexpradas.com
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/license/gpl-2.0.html
+ * Text Domain: mv-slider
+ * Domain Path: /languages
+ */
+
+ if ( ! defined( 'ABSPATH' )){
+    exit;
+ }
